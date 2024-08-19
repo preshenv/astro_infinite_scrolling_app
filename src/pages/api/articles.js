a91c2670b3d3bf5a1e7cdfdeb008da76c317ff03
preshenv <ndomaprecious95@gmail.com>
2024-08-19 21:28:22 +0100

    fix infinite scroll

--- src/pages/api/articles.js
+++ src/pages/api/articles.js
@@ -1,21 +1,22 @@
 export async function GET({ url }) {
-  const page = parseInt(url.searchParams.get("page") || "1");
-  const pageSize = parseInt(url.searchParams.get("pageSize") || "10");
+  const page = url.searchParams.get("page") || "1";
+  const pageSize = url.searchParams.get("pageSize") || "10";
 
-  const strapiResponse = await fetch(
+  const response = await fetch(
     `${
       import.meta.env.STRAPI_API_URL
     }/api/articles?pagination[page]=${page}&pagination[pageSize]=${pageSize}&populate=*`
   );
+  const data = await response.json();
 
-  const data = await strapiResponse.json();
-  const { data: articles, meta } = data;
-  const hasMore = meta.pagination.page < meta.pagination.pageCount;
-
-  return new Response(JSON.stringify({ articles, hasMore }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(
+    JSON.stringify({
+      articles: data.data,
+      hasMore: data.meta.pagination.page < data.meta.pagination.pageCount,
+      totalPages: data.meta.pagination.pageCount,
+    }),
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
